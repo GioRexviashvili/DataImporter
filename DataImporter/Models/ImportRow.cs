d4fd04eaea5c68ba4fb96f52b62166aecb9ecdd6
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-03 19:34:54 +0400

    Add ImportRow model for staging import data

---
+++ DataImporter/Models/ImportRow.cs
@@ -0,0 +1,16 @@
+namespace DataImporter.Models;
+
+public sealed record ImportRow
+{
+    public Guid BatchId { get; init; }
+    public int LineNumber { get; init; }
+
+    public string CategoryName { get; init; } = string.Empty;
+    public string CategoryIsActiveRaw { get; init; } = string.Empty;
+
+    public string ProductCode { get; init; } = string.Empty;
+    public string ProductName { get; init; } = string.Empty;
+    public string PriceRaw { get; init; } = string.Empty;
+    public string QuantityRaw { get; init; } = string.Empty;
+    public string ProductIsActiveRaw { get; init; } = string.Empty;
+}
