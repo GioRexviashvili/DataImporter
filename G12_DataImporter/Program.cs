dfe5e5ed5bbffbcd108178ce94c04e16378a7986
David Dvali <david.dvali@gmail.com>
2026-02-12 18:28:35 +0400

    Add G12_DataImporter .NET app for CSV to SQL import.

---
+++ G12_DataImporter/Program.cs
@@ -0,0 +1,31 @@
+﻿using G12_DataImporter.DataReader;
+using G12_DataImporter.Models;
+using G12_DataImporter.DataWriter;
+
+namespace G12_DataImporter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        FileInfo fileInfo = new FileInfo("/Users/irakli/Downloads/Products.txt");
+
+        CsvDataReader csvDataReader = new CsvDataReader(fileInfo);
+        IEnumerable<Category> categories = csvDataReader.GetData();
+            
+        foreach (var category in categories)
+        {
+            foreach (var product in category.Products)         
+            {
+                Console.WriteLine($"{category} {product}");
+            }
+        }
+
+        int x = Convert.ToInt16(true);
+        Console.WriteLine(x);
+
+        const string connectionString= "Server=localhost;Database=G12_ProductsCatalogue;UID=sa;PWD=Limon4ik!;Integrated Security=False; TrustServerCertificate=True";
+        SqlWriter sqlWriter = new(categories, connectionString);
+        sqlWriter.WriteData();
+    }
+}
