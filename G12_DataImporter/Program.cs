f23ba84040eea51a0e6ff61c14b67676b6c1640e
Gurami Chechelashvili <gugachechelashvili@yahoo.com>
2026-02-14 16:06:04 +0400

    asd

--- G12_DataImporter/Program.cs
+++ G12_DataImporter/Program.cs
@@ -1,25 +1,62 @@
-﻿using G12_DataImporter.DataReader;
-using G12_DataImporter.Models;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using G12_DataImporter.DataReader;
 using G12_DataImporter.DataWriter;
+using G12_DataImporter.Models;
+using Microsoft.Data.SqlClient;
 
 namespace G12_DataImporter;
 
 internal class Program
 {
-    static void Main(string[] args)
+        static int Main(string[] args)
     {
-        FileInfo fileInfo = new FileInfo(@"DataFile\Products.tsv");
+        var fileInfo = new FileInfo(Path.Combine("DataFile", "Products.tsv"));
 
-        CsvDataReader csvDataReader = new CsvDataReader(fileInfo);
-        IEnumerable<Category> categories = csvDataReader.GetData();
+        try
+        {
+            var categories = ReadCategories(fileInfo);
+            PrintCategories(categories);
 
-        foreach (Category category in categories)
+            WriteToDatabase(fileInfo);
+
+            Console.WriteLine("Data write completed.");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Fatal error: {ex.Message}");
+            return 1;
+        }
+    }
+
+    private static IList<Category> ReadCategories(FileInfo fileInfo)
+    {
+        using var csvDataReader = new CsvDataReader(fileInfo);
+        return csvDataReader.GetData().ToList();
+    }
+
+    private static void PrintCategories(IEnumerable<Category> categories)
+    {
+        foreach (var category in categories)
         {
             Console.WriteLine(category);
-            foreach (Product product in category.Products)
+            foreach (var product in category.Products)
             {
                 Console.WriteLine($"\t{product}");
             }
         }
+    }
+
+    private static void WriteToDatabase(FileInfo fileInfo)
+    {
+        const string connectionString = @"Server=.;Database=G12_ProductsCatalogue;Integrated Security=True;TrustServerCertificate=True";
+
+        using var connection = new SqlConnection(connectionString);
+        using var writerReader = new CsvDataReader(fileInfo);
+        var writer = new SqlDataWriter(connection, writerReader);
+        writer.WriteData();
     }
 }
