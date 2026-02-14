ffae6ec07ba42a70dfdbd336adc7f32e50bc2495
irakli4520 <irakli4520@gmail.com>
2026-02-14 20:50:01 +0400

    Revert "asd"
    This reverts commit f23ba84040eea51a0e6ff61c14b67676b6c1640e.

--- G12_DataImporter/Program.cs
+++ G12_DataImporter/Program.cs
@@ -1,62 +1,25 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using G12_DataImporter.DataReader;
-using G12_DataImporter.DataWriter;
+﻿using G12_DataImporter.DataReader;
 using G12_DataImporter.Models;
-using Microsoft.Data.SqlClient;
+using G12_DataImporter.DataWriter;
 
 namespace G12_DataImporter;
 
 internal class Program
 {
-        static int Main(string[] args)
+    static void Main(string[] args)
     {
-        var fileInfo = new FileInfo(Path.Combine("DataFile", "Products.tsv"));
+        FileInfo fileInfo = new FileInfo(@"DataFile\Products.tsv");
 
-        try
-        {
-            var categories = ReadCategories(fileInfo);
-            PrintCategories(categories);
+        CsvDataReader csvDataReader = new CsvDataReader(fileInfo);
+        IEnumerable<Category> categories = csvDataReader.GetData();
 
-            WriteToDatabase(fileInfo);
-
-            Console.WriteLine("Data write completed.");
-            return 0;
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine($"Fatal error: {ex.Message}");
-            return 1;
-        }
-    }
-
-    private static IList<Category> ReadCategories(FileInfo fileInfo)
-    {
-        using var csvDataReader = new CsvDataReader(fileInfo);
-        return csvDataReader.GetData().ToList();
-    }
-
-    private static void PrintCategories(IEnumerable<Category> categories)
-    {
-        foreach (var category in categories)
+        foreach (Category category in categories)
         {
             Console.WriteLine(category);
-            foreach (var product in category.Products)
+            foreach (Product product in category.Products)
             {
                 Console.WriteLine($"\t{product}");
             }
         }
-    }
-
-    private static void WriteToDatabase(FileInfo fileInfo)
-    {
-        const string connectionString = @"Server=.;Database=G12_ProductsCatalogue;Integrated Security=True;TrustServerCertificate=True";
-
-        using var connection = new SqlConnection(connectionString);
-        using var writerReader = new CsvDataReader(fileInfo);
-        var writer = new SqlDataWriter(connection, writerReader);
-        writer.WriteData();
     }
 }
