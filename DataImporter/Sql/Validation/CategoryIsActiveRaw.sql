aa42dd59fecaee7e26ea18ac584b630fd8a8caab
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-17 22:09:09 +0400

    feat: add stored procedure for CategoryIsActive field validation

---
+++ DataImporter/Sql/Validation/CategoryIsActiveRaw.sql
@@ -0,0 +1,22 @@
+create procedure CategoryIsActiveCheck_sp @BatchId uniqueidentifier
+as
+begin
+    set nocount on;
+
+    delete
+    from ImportErrors
+    where BatchId = @BatchId
+      and FieldName = N'CategoryIsActiveRaw';
+
+    insert into ImportErrors
+        (BatchId, StageRowId, LineNumber, FieldName, RawValue, Reason)
+    select s.BatchId,
+           s.Id,
+           s.LineNumber,
+           N'CategoryIsActiveRaw',
+           s.CategoryIsActiveRaw,
+           N'Expected bit value'
+    from StagingTable s
+    where s.BatchId = @BatchId
+      and try_convert(bit, ltrim(rtrim(s.CategoryIsActiveRaw))) is null
+end
