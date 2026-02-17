3987e03cabe335098070767b99379095935816b1
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-17 21:26:32 +0400

    refactor: improve SQL table scripts for better readability

--- DataImporter/Sql/CreateCategoriesTable.sql
+++ DataImporter/Sql/CreateCategoriesTable.sql
@@ -1,8 +1,9 @@
-create table Categories(
-                           ID int primary key identity(1, 1),
-                           Name nvarchar(100) not null unique,
-                           Description nvarchar(500) null,
-                           IsDeleted bit not null default(0),
-                           CreateDate datetime not null default(getdate()),
-                           UpdateDate datetime null
+create table Categories
+(
+    ID          int primary key identity (1, 1),
+    Name        nvarchar(100) not null unique,
+    Description nvarchar(500) null,
+    IsDeleted   bit           not null default (0),
+    CreateDate  datetime      not null default (getdate()),
+    UpdateDate  datetime      null
 )
