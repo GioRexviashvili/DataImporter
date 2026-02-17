e66baa2364f5fdbe231a48648a34d4ca8d39292f
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-17 19:58:34 +0400

    just a little bit modification in the names of the categories and products models

--- DataImporter/Models/Categories.cs
+++ DataImporter/Models/Categories.cs
@@ -1,15 +1,12 @@
 namespace DataImporter.Models;
 
-public class Product
+public class Categories
 {
     public int Id { get; set; }
-    public int CategoryId { get; set; }
-    public string Code { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
     public string? Description { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime? UpdateDate { get; set; }
+    public ICollection<Products> Products { get; set; } = new List<Products>();
 }
