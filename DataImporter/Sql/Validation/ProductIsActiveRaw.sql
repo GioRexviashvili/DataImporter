806934985f25115e10e07a60cad2ff72ddd8e1d6
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-17 22:16:53 +0400

    feat: add stored procedure for ProductIsActiveRaw field validation

---
+++ DataImporter/Sql/Validation/ProductIsActiveRaw.sql
@@ -0,0 +1,22 @@
+create procedure ProductIsActiveRaw_sp @BatchId uniqueidentifier
+as
+begin 
+    set nocount on;
+
+    delete
+    from ImportErrors
+    where BatchId = @BatchId
+      and FieldName = N'ProductIsActiveRaw';
+
+    insert into ImportErrors
+    (BatchId, StageRowId, LineNumber, FieldName, RawValue, Reason)
+    select s.BatchId,
+           s.Id,
+           s.LineNumber,
+           N'ProductIsActiveRaw',
+           s.ProductIsActiveRaw,
+           N'Expected bit value'
+    from StagingTable s
+    where s.BatchId = @BatchId
+      and try_convert(bit, ltrim(rtrim(s.ProductIsActiveRaw))) is null
+end
