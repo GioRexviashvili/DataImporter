44475539b691e52789e4dfef351b5e5efccf658d
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-19 03:43:30 +0400

    refactor: remove duplicate validation logic from CategoryNameValidation_sp stored procedure, category name is not unique in the batch

--- DataImporter/Sql/Validation/CategoryNameValidation_sp.sql
+++ DataImporter/Sql/Validation/CategoryNameValidation_sp.sql
@@ -3,7 +3,6 @@ as
 begin
     set nocount on;
 
-    -- check for empty or too long codes
     insert into dbo.ImportErrors
         (BatchId, StageRowId, LineNumber, FieldName, RawValue, Reason)
     select s.BatchId,
@@ -18,24 +17,4 @@ begin
         ltrim(rtrim(s.CategoryName)) = N''
             or len(ltrim(rtrim(s.CategoryName))) > 100
         );
-
-    -- check for duplicates
-    insert into ImportErrors
-        (BatchId, StageRowId, LineNumber, FieldName, RawValue, Reason)
-    select s.BatchId,
-           s.Id,
-           s.LineNumber,
-           N'CategoryName',
-           s.CategoryName,
-           N'CategoryName is not unique.'
-    from StagingTable s
-             join (select ltrim(rtrim(CategoryName)) as Name
-                   from StagingTable
-                   where BatchId = @BatchId
-                     and ltrim(rtrim(CategoryName)) <> N''
-                     and len(ltrim(rtrim(CategoryName))) <= 100
-                   group by ltrim(rtrim(CategoryName))
-                   having count(*) > 1) d
-                  on ltrim(rtrim(s.CategoryName)) = d.Name
-    where s.BatchId = @BatchId;
 end
