50447cdda148c5df31b40086b6d37471617be078
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-19 02:30:58 +0400

    feat: add stored procedure for processing and updating categories

---
+++ DataImporter/Sql/Processing/ProcessCategories_sp.sql
@@ -0,0 +1,29 @@
+create procedure ProcessCategories_sp
+as
+begin
+    set nocount on;
+
+    -- insert new categories
+    insert into Categories (Name, IsDeleted)
+    select x.CategoryName,
+           x.CategoryIsDeleted
+    from (select v.CategoryName,
+                 max(v.CategoryIsDeleted) as CategoryIsDeleted
+          from #Valid v
+          group by v.CategoryName) x
+    where not exists (select 1
+                      from Categories c
+                      where c.Name = x.CategoryName);
+
+    -- update categories only if IsDeleted changed
+    update c
+    set c.IsDeleted  = x.CategoryIsDeleted,
+        c.UpdateDate = getdate()
+    from Categories c
+             join (select v.CategoryName,
+                          max(v.CategoryIsDeleted) as CategoryIsDeleted
+                   from #Valid v
+                   group by v.CategoryName) x on x.CategoryName = c.Name
+    where c.IsDeleted != x.CategoryIsDeleted;
+end
+go
